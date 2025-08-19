fe5de46801c8dcd611e4e7d97049b251a9ccfcdd
LUCAS DA SILVA OLIVEIRA <24153691@edu119.local>
2025-08-19 15:53:50 -0300

    Atualização 19/08/25

---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import './header.css'
+// import Logo from './logo.svg'
+
+function menu() {
+    return (
+        <>
+            <header>
+                <img src="" alt="Stylewear"/>
+
+                <div>
+                    <input placeholder="Pesquisar" class="Busca"></input>
+                </div>
+
+                <a href="">Entrar</a>
+            </header>
+        </>
+    )
+};
+
+export default menu;
