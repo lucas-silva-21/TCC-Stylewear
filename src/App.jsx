dba7af05b4504c52c3dd5f5b05eab7ec488b4139
GUSTAVO SIROL MONTEIRO <24153546@edu119.local>
2025-08-21 15:47:52 -0300

    Atualizado Destaques

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Componentes/Header/Header.jsx'
 import Footer from './Componentes/Footer/Footer.jsx'
+import Crrsl from './Componentes/Destaques/Destaques.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Crrsl/>
       <Footer/>
     </>
   )
---
+++ src/Componentes/Destaques/Elementos/Elementos.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import './elementos.css'
+
+function elemento_dest(props) {
+    return (
+        <>
+            <section className="elemento">
+                <div className="part1">
+                    <a href=""><img src={props} alt="Foto1_destaque"></img></a>
+                </div>
+                <div className="part2">
+                    <h2 id="text">{props.text}</h2>
+                </div>
+            </section>
+        </>
+    )
+};
+
+export default elemento_dest;
